ca7628c67e17db10bba8fe5825f1ac2ab9bfb016
JZacharyL <47034052+JZacharyL@users.noreply.github.com>
2021-01-07 12:40:11 +0800

    First Push
    Initial Push of Game

---
+++ Mobigam_Programming_Challenge/Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+    public GameHandler gameHandler;
+    public Image timerBar;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerBar.fillAmount = 1 - (gameHandler.CurrentTime / gameHandler.MaxTime);
+
+        if(timerBar.fillAmount < 0.25f)
+        {
+            timerBar.color = Color.green;
+        }
+        else if(timerBar.fillAmount < 0.50f)
+        {
+            timerBar.color = Color.yellow;
+        }
+        else
+        {
+            timerBar.color = Color.red;
+        }
+    }
+}
