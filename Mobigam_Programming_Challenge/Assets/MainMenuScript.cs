ed0074f24957c10d212bfe7efc6446bff0eebc0d
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-01-08 14:42:29 +0800

    Added menu scene
    Package isntall: TMP

---
+++ Mobigam_Programming_Challenge/Assets/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //TODO:Change the scene callback name when publishing
+    public void StartGame()
+    {
+        SceneManager.LoadScene("gameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
