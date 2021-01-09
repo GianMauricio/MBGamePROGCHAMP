c877b63c40559d77dd92105fdcef32c1f01e0ac9
Gian Mauricio <gian_mauricio@dlsu.edu.ph>
2021-01-09 18:43:15 +0800

    Implementation: menu should now hide prev score box

--- Mobigam_Programming_Challenge/Assets/MainMenuScript.cs
+++ Mobigam_Programming_Challenge/Assets/MainMenuScript.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using Unity.Notifications.Android;
 
 public class MainMenuScript : MonoBehaviour
 {
     //TODO:Change the scene callback name when publishing
     public void StartGame()
     {
+        AndroidNotificationCenter.CancelAllDisplayedNotifications();
         SceneManager.LoadScene("gameScene");
     }
 
